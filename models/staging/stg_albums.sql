5b7e12f94777193d8be5af366321f4679dca0fab
Alex Yun <alex@phelix.ai>
2022-03-26 18:59:38 +0000

    commit for CI/CD

---
+++ models/staging/stg_albums.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='view') }}
+
+select
+    -- Identifiers
+    cast(id as int) as id,
+
+    -- Metadata
+    cast(album as string) as album,
+    cast(artist as string) as artist,
+    cast(track_length as numeric) as len_album,
+    cast(length as integer) as num_tracks,
+
+    -- Billboard 200 chart info
+    cast(`date_parsed` as date) as chart_week,
+    cast(`rank` as integer) as chart_rank
+from
+    {{ source('staging', 'albums') }}
+
+-- dbt run --m <model.sql> --var 'test_run: false'
+{% if var('test_run', default=true) %}
+    limit 100
+{% endif %}
